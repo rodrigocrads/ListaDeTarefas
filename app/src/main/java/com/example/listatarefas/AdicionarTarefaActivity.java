a05a0a1ea0a741ab181bd01bf18500e4835ea3a6
Rodrigo Cunha Rodrigues <rcunha@wkm.com.br>
2020-10-11 15:40:28 -0300

    Criar menu para Activity AdicionarTarefa

--- app/src/main/java/com/example/listatarefas/AdicionarTarefaActivity.java
+++ app/src/main/java/com/example/listatarefas/AdicionarTarefaActivity.java
@@ -3,6 +3,9 @@ package com.example.listatarefas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AdicionarTarefaActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save :
+                Toast.makeText(getApplicationContext(), "Salvando", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
